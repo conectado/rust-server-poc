c70d5ca4b56bb14813f8a5c0219875f75bb3b635
conectado <conectado2@protonmail.com>
2020-01-11 22:25:58 +0000

    Add route macro to responder to test its functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -8,10 +8,17 @@ async fn index2() -> impl Responder {
     HttpResponse::Ok().body("Hello world again!")
 }
 
+#[get("/hello")]
+async fn index3() -> impl Responder {
+    HttpResponse::Ok().body("Hello there")
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(index))
+        App::new()
+            .route("/", web::get().to(index))
             .route("/again", web::get().to(index2))
+            .service(index3)
     }).bind("127.0.0.1:8888")?.run().await
 }
